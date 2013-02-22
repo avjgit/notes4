16d014cec12e26fee48a712014c64cfe234b3996
avjgit <avjmail@gmail.com>
2013-02-22 19:02:58 +0200

    lu automata prg init

---
+++ lu/semester2/automata/homework1.rb
@@ -0,0 +1,10 @@
+##################################################################################
+# 1) Noprogrammēt automātu, kas akceptē valodu LA={v | v sākas ar 0 un beidzas ar 00} alfabētā Σ={0, 1}
+# –Drīkst izmantot jebkādu programmēšanas valodu
+# –Programmā jābūt funkcijai, kas saņem ievadā simbolu virkni (string) un atgriež jā vai nē (boolean)
+# –Jāiesniedz tikai pirmkods
+
+
+
+##################################################################################
+# 2) Neformāli aprakstīt vispārīgus principus, kā noprogrammēt patvaļīgu automātu.
