536c561c89daa03bf9e65630dd790e0a459ccef1
avjgit <avjmail@gmail.com>
2013-03-12 01:03:25 +0200

    lu programming2 hw1 end_of_word

--- lu/semester2/programming/f.cpp
+++ lu/semester2/programming/f.cpp
@@ -2,11 +2,54 @@
 // F10. Doti divi faili f1 un f2. Failā f1 ir patvaļīgs teksts, failā f2 ir ne vairāk kā 40 vārdu pārīšu, kas atdalīti ar komatiem. Izdrukāt failā tekstu no ieejas faila f1, kurā visi vārdi, kas ir sastopami failā f2 kā pirmā komponente no pārīša, tiek aizstāti ar tam atbilstošo failā f2 norādīto vārdu. Drīkst uzskatīt, ka vārda garums failā nepārsniedz 40 simbolus. Par vārdu uzskatīt patvaļīgu simbolu virkni, kas atdalīta ar tukšumiem vai pieturas zīmēm (punkts, komats, apaļās iekavas, izsaukuma zīme, jautājuma zīme).
 // ========================================================
 #include <stdio.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+bool is_end_of_word(char c)
+{
+    char separators[] = {' ', '.', ',', '(', ')', '!', '?'};
+    int arrSize = sizeof(separators) / sizeof(char);
+    for (int i = 0; i < arrSize; i++)
+    {
+        if (separators[i] == c)
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main()
 {
-    // cout << "wtf";
-    // system("pause");
-    printf("asf");
+    // tests of is_end_of_word
+    if (is_end_of_word('a')) {cout << "nok";} else {cout << "ok";}
+    if (is_end_of_word(' ')) {cout << "ok";} else {cout << "nok";}
+    if (is_end_of_word('?')) {cout << "ok";} else {cout << "nok";}
+
+
+
+    // end of tests of is_end_of_word
+
+    // char character;
+    // char word[40];
+    // ifstream inFile;
+    // inFile.open("f1");
+    // if(!inFile) return 1;
+
+    // while(inFile)
+    // {
+    //     inFile >> c;
+    //     cout << c;
+    // }
+    // // while (!inFile.eof())
+    // // {
+    // //     inFile >> c;
+    // //     cout << c;
+    // // }
+    // printf("asf");
+    // inFile.close();
+
     return 0;
 }
