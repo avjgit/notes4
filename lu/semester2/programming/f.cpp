b1503ef989b8c99fccde09b9f454749f36cbef33
AJ <avjmail@gmail.com>
2013-03-26 18:34:51 +0200

    lu prg2: task F: init

---
+++ lu/semester2/programming/f.cpp
@@ -0,0 +1,15 @@
+// task:
+// Izveidot programmu valodā C++, kas apstrādā teksta failu secīgā režīmā.
+// Dati no faila jānolasa pa vienam simbolam (nedrīkst nolasīt uzreiz visu failu vai veselu rindiņu).
+// Nedrīkst dublēt visa faila saturu operatīvajā atmiņā.
+// F10. Doti divi faili f1 un f2.
+// Failā f1 ir patvaļīgs teksts,
+// failā f2 ir ne vairāk kā 40 vārdu pārīšu,
+// kas atdalīti ar komatiem.
+// Izdrukāt failā tekstu no ieejas faila f1,
+// kurā visi vārdi, kas ir sastopami failā f2 kā pirmā komponente no pārīša,
+// tiek aizstāti ar tam atbilstošo failā f2 norādīto vārdu.
+// Drīkst uzskatīt, ka vārda garums failā nepārsniedz 40 simbolus.
+// Par vārdu uzskatīt patvaļīgu simbolu virkni,
+// kas atdalīta ar tukšumiem vai pieturas zīmēm
+// (punkts, komats, apaļās iekavas, izsaukuma zīme, jautājuma zīme).
