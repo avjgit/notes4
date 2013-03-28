bd2b9563955a6ee9dc4b39791b55683e953d3679
AJ <avjmail@gmail.com>
2013-03-28 18:55:43 +0200

    lu programming2 hw1 returned good things

--- lu/semester2/programming/f.cpp
+++ lu/semester2/programming/f.cpp
@@ -18,20 +18,38 @@
 #include <fstream>
 using namespace std;
 
+bool is_end_of_word(char c)
+{
+    char separators[] = {' ', '.', ',', '(', ')', '!', '?'};
+    int arrSize = sizeof(separators) / sizeof(char);
+    for (int i = 0; i < arrSize; i++)
+    {
+        if (separators[i] == c)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
-    char c;
+    // tests of is_end_of_word
+    // if (is_end_of_word('a')) {cout << "nok";} else {cout << "ok";}
+    // if (is_end_of_word(' ')) {cout << "ok";} else {cout << "nok";}
+    // if (is_end_of_word('?')) {cout << "ok";} else {cout << "nok";}
+
+    char c;        // character to read in
+    char word[40]; // array to save a word read in
     fstream f1 ("f1");
     fstream f2 ("f2");
 
     c = f1.get();
     while (!f1.eof()) {
         cout << c;
+        f2 << c;
         c = f1.get();
     }
-
-
-
 
     f1.close();
     f2.close();
