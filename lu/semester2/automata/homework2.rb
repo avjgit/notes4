324f6f6eef7a824e881860c128e61039d523a05d
avjgit <avjmail@gmail.com>
2013-04-11 21:49:49 +0300

    lu automata hw1 init

---
+++ lu/semester2/automata/homework2.rb
@@ -0,0 +1,9 @@
+# TASK:
+# 1)Uzbūvēt ε-avotu, kas akceptē, tos un tikai tos vārdus,
+# kas pieder valodai L={w┤|  w satur abus fragmentus gulbis un nadals} alfabētā Σ=ASCII.
+# 2)Determinizēt uzbūvēto avotu.
+# 3)Noprogrammēt uzbūvēto automātu:
+# –Drīkst izmantot jebkādu programmēšanas valodu
+# –Nedrīkst izmantot jebkāda veida bibliotēkas, kas meklē tekstā, piemēram, regulārās izteiksmes.
+# –Programmā jābūt funkcijai, kas saņem ievadā simbolu virkni (string) un atgriež sarakstu ar visām fragmenta gulbis beigu simbola atrašanās pozīcijām simbolu virknē.
+# Jāiesniedz: ε-avots, determinēts automāts, pirmkods
