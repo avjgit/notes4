4206e12ac4b38ed8a2c4d27a237d0f08ec944aac
avjgit <avjmail@gmail.com>
2013-04-11 21:54:29 +0300

    lu automata hw2: direction 1

--- lu/semester2/automata/homework2.rb
+++ lu/semester2/automata/homework2.rb
@@ -7,3 +7,9 @@
 # –Nedrīkst izmantot jebkāda veida bibliotēkas, kas meklē tekstā, piemēram, regulārās izteiksmes.
 # –Programmā jābūt funkcijai, kas saņem ievadā simbolu virkni (string) un atgriež sarakstu ar visām fragmenta gulbis beigu simbola atrašanās pozīcijām simbolu virknē.
 # Jāiesniedz: ε-avots, determinēts automāts, pirmkods
+# -----------------------------------------------------
+# 1)Uzbūvēt ε-avotu, kas akceptē, tos un tikai tos vārdus,
+# kas pieder valodai L={w┤|  w satur abus fragmentus gulbis un nadals} alfabētā Σ=ASCII.
+
+==> (a)
+        -Σ->(a)
