0667008d1eb97ff382ee5e980bd376e227e026b7
avjgit <avjmail@gmail.com>
2013-04-13 11:56:43 +0300

    lu prg2 exH: init

---
+++ lu/semester2/programming/h.cpp
@@ -0,0 +1,42 @@
+// H6. Uzrakstīt programmu, kas ļauj izveidot un labot bināru failu,
+// kura ieraksta struktūra ir sekojoša:
+// vārds – atslēga (30 simboli),
+// vārds – vērtība (30 simboli),
+// ieraksta statuss (0 vai 1).
+// Programmai katrs jauns ieraksts jāieliek faila beigās. Jāparedz iespēja
+// (1) izmest faila komponenti (loģiski atzīmējot kā izmestu),
+// (2) izdrukāt faila esošās komponentes uz ekrāna,
+// (3) izmest loģiski izmestas komponentes fiziski.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// (1) izmest faila komponenti (loģiski atzīmējot kā izmestu),
+void mark_inactive()
+{
+
+}
+
+// (2) izdrukāt faila esošās komponentes uz ekrāna,
+void print()
+{
+
+}
+
+// (3) izmest loģiski izmestas komponentes fiziski.
+void remove_inactive()
+{
+
+}
+
+int main()
+{
+    const int key_size      = 30;
+    const int value_size    = 30;
+    const int flag_size     = 1;
+    const int flag_active   = 1;
+    const int flag_inactive = 0;
+
+    return 0;
+}
