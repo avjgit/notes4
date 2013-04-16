8aafb10c05ca64e48d58347fbec9eb0a00ca1ee6
avjgit <avjmail@gmail.com>
2013-04-16 04:19:38 +0300

    lu prg2 exG: fix

--- lu/semester2/programming/g.cpp
+++ lu/semester2/programming/g.cpp
@@ -4,7 +4,8 @@
 //   b) jāpielieto uzrakstītā funkcija sarakstam,
 //   c) jāizdrukā saraksts pēc funkcijas darbības.
 //   d) beigās jāiznīcina saraksts (lietojot delete).
-// G9. Uzrakstīt funkciju, kas pārvieto saraksta n-to elementu uz saraksta beigām. Darbība jāveic, pārkabinot saites, nevis pārrakstot elementu vērtības.
+// G9. Uzrakstīt funkciju, kas pārvieto saraksta n-to elementu uz saraksta beigām.
+// Darbība jāveic, pārkabinot saites, nevis pārrakstot elementu vērtības.
 #include <iostream>
 using namespace std;
 
@@ -18,13 +19,19 @@ int main()
 
     element* first      = NULL;
     element* last       = NULL;
+
+    element* before     = NULL;
     element* current    = NULL;
+    element* after      = NULL;
+
     string buffer;
 
     const char menu_add    = 'a';
     const char menu_print  = 'p';
+    const char menu_move   = 'm';
     const char menu_quit   = 'q';
           char menu_chosen = ' ';
+    int movable;
 
     while (menu_chosen != menu_quit)
     {
@@ -34,6 +41,7 @@ int main()
         cout << "-------------------" << endl;
         cout << "Enter '" << menu_add               << "' to (a)dd new entry"               << endl;
         cout << "Enter '" << menu_print             << "' to (p)rint all entries"           << endl;
+        cout << "Enter '" << menu_move              << "' to (m)ove entry to the end"       << endl;
         cout << "Enter '" << menu_quit              << "' to (q)uit"                        << endl;
         cout << "Your choice: ";
         cin >> menu_chosen;
@@ -64,6 +72,39 @@ int main()
                 {
                     cout << "- " << current->content << endl;
                 }
+                break;
+            case menu_move:
+                cout << "Which list item to move: ";
+                cin >> movable;
+
+                before = first;
+                current = first;
+                after = current->next;
+
+                for(int i = 1; i < movable; i++)
+                {
+                    before = current;
+                    current = after;
+                    after = current->next;
+                }
+
+                // change link before straight to after (skip "current")
+                if (after != NULL)
+                {
+                    before->next = after;
+                }
+
+                // if 1st?
+                if (movable == 1)
+                {
+                    first = after;
+                }
+
+                last->next = current;
+                last = last->next;
+
+                current->next = NULL;
+
                 break;
             case menu_quit:
                 break;
