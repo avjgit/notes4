1de2b2f281de2ecef4e07e085571c876dfac62fe
avjgit <avjmail@gmail.com>
2013-04-18 09:34:42 +0300

    lu db hw1 init

--- lu/semester2/db/homework1.sql
+++ lu/semester2/db/homework1.sql
@@ -1,19 +1,19 @@
--- Izveidot ER modeli Čena sintaksē.
--- Uzrakstīt tabulu izveides skriptu atbilstoši izveidotajam ER modelim.
--- Uzrakstīt prasīto SQL pieprasījumu jūsu izveidotajai tabulu struktūrai.
--- Uzdevuma risinājums jāsūta uz adresi girts.karnitis@lu.lv.
--- Epasta subj laukam jābūt formā DBI MD1 aj05044, kur AA9999 ir jūsu studentu apliecības numurs.
--- Mājas darba tekstam ir obligāti jāsatur jūsu vārds, uzvārds un studenta apliecības numurs.
+-- To: girts.karnitis@lu.lv Subj: DBI MD1 aj05044
 
--- Transporta līdzekļu reisu uzskaite:
+-- DBI MD1 aj05044 Andrejs Jurčenoks
+
+-- TRANSPORTA LĪDZEKĻU REISU UZSKAITE:
+-- Uzrakstīt tabulu izveides skriptu atbilstoši izveidotajam ER modelim:
 
 -- Jāglabā informācija par transporta līdzekļiem – Valsts reģistrācijas Nr. (unikāls), ražotājs, modelis, izlaiduma gads, krāsa.
 
 -- Jāglabā informācija par šoferiem – Personas kods (unikāls), vārds, uzvārds, vadītāja apliecības nr.
 
--- Katru transporta līdzekli atļauts vadīt vairākiem šoferiem, katram šoferiem atļauts vadīt vairākus transporta līdzekļus.
 -- Jāsaglabā ziņas, kuru transporta līdzekli kuriem šoferiem ir atļauts vadīt.
+-- Katru transporta līdzekli atļauts vadīt vairākiem šoferiem, katram šoferiem atļauts vadīt vairākus transporta līdzekļus.
 
 -- Jāglabā ziņas par reisiem – kurā datumā no kurienes uz kurieni kurš šoferis ar kuru transporta līdzekli ir braucis, cik km nobraucis šī reisa laikā.
 
 -- Atlasīt informāciju par visiem reisiem, kas notikuši 2012. gadā – reisa datums, No kurienes, uz kurieni, cik km nobraukti šī reisa laikā.
+
+-- Izveidot ER modeli Čena sintaksē.
