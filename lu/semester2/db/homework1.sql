44b68b0c3a7957c0fb4dd318c65eba6a9fd0f85a
avjgit <avjmail@gmail.com>
2013-04-18 09:32:17 +0300

    lu db hw1 init

---
+++ lu/semester2/db/homework1.sql
@@ -0,0 +1,19 @@
+-- Izveidot ER modeli Čena sintaksē.
+-- Uzrakstīt tabulu izveides skriptu atbilstoši izveidotajam ER modelim.
+-- Uzrakstīt prasīto SQL pieprasījumu jūsu izveidotajai tabulu struktūrai.
+-- Uzdevuma risinājums jāsūta uz adresi girts.karnitis@lu.lv.
+-- Epasta subj laukam jābūt formā DBI MD1 aj05044, kur AA9999 ir jūsu studentu apliecības numurs.
+-- Mājas darba tekstam ir obligāti jāsatur jūsu vārds, uzvārds un studenta apliecības numurs.
+
+-- Transporta līdzekļu reisu uzskaite:
+
+-- Jāglabā informācija par transporta līdzekļiem – Valsts reģistrācijas Nr. (unikāls), ražotājs, modelis, izlaiduma gads, krāsa.
+
+-- Jāglabā informācija par šoferiem – Personas kods (unikāls), vārds, uzvārds, vadītāja apliecības nr.
+
+-- Katru transporta līdzekli atļauts vadīt vairākiem šoferiem, katram šoferiem atļauts vadīt vairākus transporta līdzekļus.
+-- Jāsaglabā ziņas, kuru transporta līdzekli kuriem šoferiem ir atļauts vadīt.
+
+-- Jāglabā ziņas par reisiem – kurā datumā no kurienes uz kurieni kurš šoferis ar kuru transporta līdzekli ir braucis, cik km nobraucis šī reisa laikā.
+
+-- Atlasīt informāciju par visiem reisiem, kas notikuši 2012. gadā – reisa datums, No kurienes, uz kurieni, cik km nobraukti šī reisa laikā.
