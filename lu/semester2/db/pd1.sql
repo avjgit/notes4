e3ff256c9b34fa82b5e33fbb61f71ee09072a8fb
avjgit <avjmail@gmail.com>
2013-05-15 19:01:01 +0300

    lu db1 INSERTS

--- lu/semester2/db/pd1.sql
+++ lu/semester2/db/pd1.sql
@@ -9,12 +9,12 @@
 -- IzlaidumaGads   Vesels skaitlis     Nav Null
 -- Ipasnieks   Simbolu virkne ar mainīgu garumu    50  Var būt Null
 
-CREATE TABLE [automasina] (
+CREATE TABLE [Automasina] (
      RegNr          [char]    (10) NOT NULL PRIMARY KEY
     ,Marka          [varchar] (30) NOT NULL
     ,Modelis        [varchar] (25) NOT NULL
     ,IzlaidumaGads  [int]          NOT Null
-    ,Ipasnieks      [varchar] (50) NULL
+    ,Ipasnieks      [varchar] (50) COLLATE Latvian_CI_AI NULL
 )
 GO
 
@@ -26,18 +26,18 @@ GO
 -- Datums  Datums      Nav Null
 -- AutoRegNr   Simbolu virkne ar fiksētu garumu    10  Ārējā atslēga. Norāde uz tabulu Automasina lauku RegNr
 
-CREATE TABLE [sods] (
+CREATE TABLE [Sods] (
      ID         [int] IDENTITY(1,1) NOT NULL PRIMARY KEY
     ,Izmers     [numeric] (4,2) NOT NULL
-    ,Parkapums  [varchar] (100) NULL
+    ,Parkapums  [varchar] (100) COLLATE Latvian_CI_AI NULL
     ,Datums     [datetime] NOT NULL
     ,AutoRegNr  [char]    (10)
 )
 GO
 
-ALTER TABLE [sods]
+ALTER TABLE [Sods]
 ADD  CONSTRAINT [fk_sods_automasina_regnr] FOREIGN KEY([AutoRegNr])
-REFERENCES [automasina] ([RegNr])
+REFERENCES [Automasina] ([RegNr])
 GO
 
 -- Automasina
@@ -47,12 +47,18 @@ GO
 -- FU23    BMW 530 2011
 -- HK9559  Citroen C5  2011    Jānis Zaļenieks
 
--- Automasina
--- RegNr   Marka   Modelis IzlaidumaGads   Ipasnieks
--- GT9354  Audi    A4  2010    Mērija Popinsa
--- BU9838  Opel    Vectra  2006    Mākoņpūka SIA
--- FU23    BMW 530 2011
--- HK9559  Citroen C5  2011    Jānis Zaļenieks
+INSERT INTO Automasina (RegNr, Marka, Modelis, IzlaidumaGads, Ipasnieks)
+VALUES('GT9354',  'Audi',    'A4',  2010,    'Mērija Popinsa')
+
+INSERT INTO Automasina (RegNr, Marka, Modelis, IzlaidumaGads, Ipasnieks)
+VALUES('BU9838',  'Opel',    'Vectra',  2006,    'Mākoņpūka SIA')
+
+INSERT INTO Automasina (RegNr, Marka, Modelis, IzlaidumaGads, Ipasnieks)
+VALUES('FU23',    'BMW', '530', 2011, NULL)
+
+INSERT INTO Automasina (RegNr, Marka, Modelis, IzlaidumaGads, Ipasnieks)
+VALUES('HK9559',  'Citroen', 'C5'  2011,    'Jānis Zaļenieks')
+
 
 -- Sods
 -- Izmers  Parkapums   Datums  AutoRegNr
@@ -61,12 +67,17 @@ GO
 -- 200.00      09.04.2013  BU9838
 -- 10.00   Neatļauta pagrieziena veikšana  14.03.2012  GT9354
 
--- Sods
--- Izmers  Parkapums   Datums  AutoRegNr
--- 5.00    Stāvēšana neatļautā vietā   11.02.2013  GT9354
--- 40.00   Ātruma pārkāpšana   05.06.2012  HK9559
--- 200.00      09.04.2013  BU9838
--- 10.00   Neatļauta pagrieziena veikšana  14.03.2012  GT9354
+INSERT INTO Sods (Izmers,  Parkapums,   Datums,  AutoRegNr)
+VALUES (5.00,    'Stāvēšana neatļautā vietā',   11.02.2013,  'GT9354')
+
+INSERT INTO Sods (Izmers,  Parkapums,   Datums,  AutoRegNr)
+VALUES (40.00,   'Ātruma pārkāpšana',   05.06.2012, 'HK9559'
+
+INSERT INTO Sods (Izmers,  Parkapums,   Datums,  AutoRegNr)
+VALUES (200.00,  NULL,    09.04.2013,  'BU9838'
+
+INSERT INTO Sods (Izmers,  Parkapums,   Datums,  AutoRegNr)
+VALUES (10.00,   'Neatļauta pagrieziena veikšana',  14.03.2012,  'GT9354'
 
 -- Izmainīt datus tabulā Automasina – Automašīnai  FU23 nomainīt īpašnieku  uz „Anna Pētersone”.
 -- Izmainīt datus tabulā Automasina – Automašīnai  FU23 nomainīt īpašnieku  uz „Anna Pētersone”.
