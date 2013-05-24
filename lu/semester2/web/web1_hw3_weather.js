f94ea9e5c14e05676e42cf0187a1ba47b0336502
avjgit <avjmail@gmail.com>
2013-05-24 23:37:01 +0300

    lu webI hw3-weather init

---
+++ lu/semester2/web/web1_hw3_weather.js
@@ -0,0 +1,17 @@
+//The task: allow the user to input weather forecast information, then create a graphic display of the forecast.
+
+//when the user clicks "Generate graphics",
+//create a weather forecast on the "canvas" element,
+//showing weather information of each day
+//The implementation is up to you.
+// You may use image icons for cloudiness and wind directions,
+// you may use colors for temperatures,
+// you may draw trend lines for temperature changes, ...
+
+//Before drawing anything, check if the inputs are appropriate:
+// TODO - temperatures should be numeric values (both positive and negative, but the value is somewhat limited between -50 and 50deg C)
+// TODO - wind speed should not be negative, but can be zero
+// TODO - cloudiness value should be picked
+
+//Bonus task: calculate the "weather feels like" value.
+// Some good ideas can be found here: http://donsnotes.com/reference/temperature-index.html
