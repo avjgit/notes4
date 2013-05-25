39b82e9719ea76aac5897bd0ffefd8928db9a6c1
avjgit <avjmail@gmail.com>
2013-05-25 20:58:52 +0300

    lu webI hw3-weather extracted validation

--- lu/semester2/web/web1_hw3_weather.js
+++ lu/semester2/web/web1_hw3_weather.js
@@ -1,8 +1,59 @@
 window.onload = mainFunction;
 
 function getInputs(id, tag){
-    tag = typeof tag !== 'undefined' ? tag : "input";
+    tag = typeof tag !== 'undefined' ? tag : "input"; // default tag - input
     return document.getElementById(id).getElementsByTagName(tag);
+}
+
+function validate_temperatures(temperatures){
+    var i;
+    var min_temperature = -50;
+    var max_tempearture = +50;
+    for(i = 0; i < temperatures.length; i++){
+        alert(temperatures[i].value);
+
+        if (temperatures[i].value === 'undefined' ||
+           temperatures[i].value === '') {
+            alert("temperatures should be provided!");
+        }
+        if (isNaN(temperatures[i].value)) {
+            alert("temperatures should be a number!");
+        }
+        if (temperatures[i].value < min_temperature ||
+           temperatures[i].value > max_tempearture) {
+            alert("temperatures should be from "
+                + min_temperature + " to "
+                + max_tempearture + " degC!"
+            );
+        }
+    }
+}
+
+function validate_windspeeds(windspeeds){
+    var i;
+    for(i = 0; i < windspeeds.length; i++){
+
+        if (windspeeds[i].value === 'undefined' ||
+           windspeeds[i].value === '') {
+            // lets set default for wind
+           windspeeds[i] = 0;
+        }
+        if (isNaN(windspeeds[i].value)) {
+            alert("windspeed should be a number!");
+        }
+        if (windspeeds[i].value < 0) {
+            alert("windspeed should be non-negative!");
+        }
+    }
+}
+
+function validate_clouds(sunclouds){
+    var i;
+    for(i = 0; i < sunclouds.length; i++){
+        if (sunclouds[i].selectedIndex === 0) {
+            alert("clouds should be specified!");
+        }
+    }
 }
 
 function mainFunction(){
@@ -12,42 +63,9 @@ function mainFunction(){
         var windspeeds   = getInputs("windspeed");
         var sunclouds    = getInputs("sunclouds", "select");
 
-        var i;
-        for(i = 0; i < temperatures.length; i++){
-            alert(temperatures[i].value);
+        validate_temperatures(temperatures);
+        validate_windspeeds(windspeeds);
+        validate_clouds(sunclouds);
 
-            if (temperatures[i].value == 'undefined' ||
-               temperatures[i].value === '') {
-                alert("t should be provided!");
-            }
-            if (isNaN(temperatures[i].value)) {
-                alert("t should be a number!");
-            }
-            if (temperatures[i].value < -50 ||
-               temperatures[i].value > 50) {
-                alert("t should be from -50 to 50 degC!");
-            }
-        }
-
-        for(i = 0; i < windspeeds.length; i++){
-
-            if (windspeeds[i].value == 'undefined' ||
-               windspeeds[i].value === '') {
-                // lets set default for wind
-               windspeeds[i] = 0;
-            }
-            if (isNaN(windspeeds[i].value)) {
-                alert("ws should be a number!");
-            }
-            if (windspeeds[i].value < 0) {
-                alert("ws should be non-negative!");
-            }
-        }
-
-         for(i = 0; i < sunclouds.length; i++){
-            if (sunclouds[i].selectedIndex === 0) {
-                alert("cl should be specified!");
-            }
-        }
     };
 }
