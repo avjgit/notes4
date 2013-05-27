14b5337d6584457372ffbb59e1cde9dd83ef70a4
avjgit <avjmail@gmail.com>
2013-05-27 23:06:05 +0300

    lu algorithms parents and children

--- lu/semester2/algorithms/03_komanda/komanda.cpp
+++ lu/semester2/algorithms/03_komanda/komanda.cpp
@@ -4,7 +4,7 @@
 struct node
 {
     long long self, left_nr, right_nr;
-    node *next, *parent, *left, *right;
+    node *next, *left, *right;
 };
 
 int main()
@@ -28,6 +28,8 @@ int main()
         current->left_nr        = left;
         current->right_nr       = right;
         current->next           = NULL;
+        current->left           = NULL;
+        current->right          = NULL;
 
         if (first == NULL)
         {
@@ -42,51 +44,95 @@ int main()
     }
 
     ///////////////////////////////////////////////// fill in childs and parent
-    // for(node *parent = first; parent != NULL; parent = parent->next)
-    // {
-    //     for(node *child = first->next; child != NULL; child = child->next)
-    //     {
-    //         if (child->self = parent->left_nr)
-    //         {
-    //             parent->left = child;
-    //             break;
-    //         }
-    //         if (child->self = parent->right_nr)
-    //         {
-    //             parent->right = child;
-    //             break;
-    //         }
-    //     }
-    // }
+    for(node *parent = first; parent != NULL; parent = parent->next)
+    {
+        // fprintf(stdout,"-------- in with parent %lld\n", parent->self);
+        for(node *child = first; child != NULL; child = child->next)
+        {
+            // fprintf(stdout,"checking child %lld\n", child->self);
+            if (child->self == parent->left_nr)
+            {
+                parent->left = child;
+                break;
+            }
+        }
 
-    // node *maybe_parent = NULL;
-    // node *root = NULL;
-    // for(node *child = first; child != NULL; child = child->next)
-    // {
-    //     for(maybe_parent = first->next; maybe_parent != NULL; maybe_parent = maybe_parent->next)
-    //     {
-    //         if (maybe_parent->left_nr  == child->self||
-    //             maybe_parent->right_nr == child->self)
-    //         {
-    //             break;
-    //         }
-    //     }
-    //     if (maybe_parent = NULL)
-    //     {
-    //         root = child;
-    //         break;
-    //     }
-    // }
+        if (parent->left == NULL)
+        {
+            current = new node;
+            current->self           = parent->left_nr;
+            current->left           = NULL;
+            current->right          = NULL;
+            parent->left = current;
+        }
+
+        for(node *child = first; child != NULL; child = child->next)
+        {
+            // fprintf(stdout,"checking child %lld\n", child->self);
+            if (child->self == parent->right_nr)
+            {
+                parent->right = child;
+                break;
+            }
+        }
+
+        if (parent->right == NULL)
+        {
+            current = new node;
+            current->self           = parent->right_nr;
+            current->left           = NULL;
+            current->right          = NULL;
+            parent->right = current;
+        }
+    }
+
+    node *maybe_parent = NULL;
+    node *root = NULL;
+    for(node *child = first; child != NULL; child = child->next)
+    {
+        // fprintf(stdout,"-------- in with child %lld\n", child->self);
+        for(maybe_parent = first; maybe_parent != NULL; maybe_parent = maybe_parent->next)
+        {
+            // fprintf(stdout,"checking if parent %lld\n", maybe_parent->self);
+            if (maybe_parent->left_nr  == child->self||
+                maybe_parent->right_nr == child->self)
+            {
+                break;
+            }
+        }
+
+        if (maybe_parent == NULL)
+        {
+            // fprintf(stdout, "inside root\n");
+            root = child;
+            break;
+        }
+    }
+    // fprintf(stdout, "root is %lld\n", root->self);
+
 
     ///////////////////////////////////////////////// output
+    node *left_node = NULL;
+    node *right_node = NULL;
+    long long left_node_self, right_node_self;
+
     for(current = first; current != NULL; current = current->next)
     {
-        fprintf (
+        // fprintf(stdout, "current: %lld\n", current->self);
+
+        left_node = current->left;
+        left_node_self = left_node->self;
+
+        right_node = current->right;
+        right_node_self = right_node->self;
+
+        fprintf(
             out,
             "%lld: %lld %lld\n",
             current->self,
-            current->left_nr,
-            current->right_nr);
+            left_node_self,
+            right_node_self
+        );
     }
 
     ///////////////////////////////////////////////// closing
