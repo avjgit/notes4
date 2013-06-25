130988ad87fefb3a4ee24511c8faa31162d1b1c2
avjgit <avjmail@gmail.com>
2013-06-25 23:53:03 +0300

    lu alogrithms: lab clock: refactored line

--- lu/semester2/algorithms/digitclock.cpp
+++ lu/semester2/algorithms/digitclock.cpp
@@ -11,11 +11,15 @@ public:
     Clock(){
         size = 1;
     }
-    void print0(){
-        //upper line
+    void line()
+    {
         cout << " ";
         for (int i = 0; i < size; i++) cout << "-";
         cout << " " << endl;
+    }
+    void print0(){
+        //upper line
+        line();
         //first half
         for (int i = 0; i < size; i++){
             cout << "|";
@@ -33,9 +37,7 @@ public:
             cout << "|" << endl;
         }
         // bottom line
-        cout << " ";
-        for (int i = 0; i < size; i++) cout << "-";
-        cout << " " << endl;
+        line();
     }
     void print1(){
         //upper line
@@ -63,11 +65,89 @@ public:
         for (int i = 0; i < size; i++) cout << " ";
         cout << "|" << endl;
     }
+    void print2(){
+        //upper line
+        // cout << "-";
+        // for (int i = 0; i < size; i++) cout << "-";
+        // cout << "-" << endl;
+        line();
+
+        //first half
+        for (int i = 0; i < size; i++){
+            cout << " ";
+            for (int i = 0; i < size; i++) cout << " ";
+            cout << "|" << endl;
+        }
+        //middle line
+        // cout << "-";
+        // for (int i = 0; i < size; i++) cout << "-";
+        // cout << "-" << endl;
+        line();
+
+        //second half
+        for (int i = 0; i < size; i++){
+            cout << "|";
+            for (int i = 0; i < size; i++) cout << " ";
+            cout << " " << endl;
+        }
+        // bottom line
+        line();
+
+    }
+    void print3(){
+        //upper line
+        line();
+        //first half
+        for (int i = 0; i < size; i++){
+            cout << " ";
+            for (int i = 0; i < size; i++) cout << " ";
+            cout << "|" << endl;
+        }
+        //middle line
+        line();
+        //second half
+        for (int i = 0; i < size; i++){
+            cout << " ";
+            for (int i = 0; i < size; i++) cout << " ";
+            cout << "|" << endl;
+        }
+        // bottom line
+        line();
+
+    }
+    void print4(){
+        //upper line
+        cout << "|";
+        for (int i = 0; i < size; i++) cout << " ";
+        cout << "|" << endl;
+        //first half
+        for (int i = 0; i < size; i++){
+            cout << "|";
+            for (int i = 0; i < size; i++) cout << " ";
+            cout << "|" << endl;
+        }
+        //middle line
+        line();
+
+        //second half
+        for (int i = 0; i < size; i++){
+            cout << " ";
+            for (int i = 0; i < size; i++) cout << " ";
+            cout << "|" << endl;
+        }
+        // bottom line
+        cout << " ";
+        for (int i = 0; i < size; i++) cout << " ";
+        cout << " " << endl;
+    }
 };
 
 int main(){
     Clock clock;
     clock.print0();
     clock.print1();
+    clock.print2();
+    clock.print3();
+    clock.print4();
     return 0;
 }
