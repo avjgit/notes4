4f6273f0ee091b106ba6c9e8e7eb4ac862fddc70
avjgit <avjmail@gmail.com>
2013-06-26 01:52:33 +0300

    lu alogrithms: lab clock: formatting

--- lu/semester2/algorithms/digitclock.cpp
+++ lu/semester2/algorithms/digitclock.cpp
@@ -103,30 +103,20 @@ public:
         cout << endl;
     }
 
-    void empty_line(){
-        for (int i = 0; i < size; i++) cout << " ";
-    }
-    void full_line(){
-        for (int i = 0; i < size; i++) cout << "-";
-    }
-    void line(){
-        // cout << " " << full_line() << " ";
-        cout << " "; full_line(); cout << " ";
-    }
-    void double_line(){
-        cout << "|"; empty_line(); cout << "|";
-    }
-    void right_line(){
-        cout << " "; empty_line(); cout << "|";
-    }
-    void left_line(){
-        cout << "|"; empty_line(); cout << " ";
-    }
+    void empty_line()   {for (int i = 0; i < size; i++) cout << " ";}
+    void full_line()    {for (int i = 0; i < size; i++) cout << "-";}
+
+    void line()         {cout << " "; full_line();  cout << " ";}
+    void double_line()  {cout << "|"; empty_line(); cout << "|";}
+    void right_line()   {cout << " "; empty_line(); cout << "|";}
+    void left_line()    {cout << "|"; empty_line(); cout << " ";}
+
     void print0_upper()     {line();}
     void print0_uphalf()    {double_line();}
     void print0_middle()    {double_line();}
     void print0_downhalf()  {double_line();}
     void print0_bottom()    {line();}
+
     void print1_upper()     {right_line();}
     void print1_uphalf()    {right_line();}
     void print1_middle()    {right_line();}
@@ -139,19 +129,18 @@ public:
     void print2_downhalf()  {left_line();}
     void print2_bottom()    {line();}
 
-
     void print3_upper()     {line();}
     void print3_uphalf()    {right_line();}
     void print3_middle()    {line();}
     void print3_downhalf()  {right_line();}
     void print3_bottom()    {line();}
 
-
     void print4_upper()     {double_line();}
     void print4_uphalf()    {double_line();}
     void print4_middle()    {line();}
     void print4_downhalf()  {right_line();}
     void print4_bottom()    {right_line();}
+
     void print5_upper()     {line();}
     void print5_uphalf()    {left_line();}
     void print5_middle()    {line();}
@@ -164,21 +153,17 @@ public:
     void print6_downhalf()  {line();}
     void print6_bottom()    {line();}
 
-
     void print7_upper()     {line();}
     void print7_uphalf()    {line();}
     void print7_middle()    {line();}
     void print7_downhalf()  {line();}
     void print7_bottom()    {line();}
 
-
     void print8_upper()     {line();}
     void print8_uphalf()    {line();}
     void print8_middle()    {line();}
     void print8_downhalf()  {line();}
     void print8_bottom()    {line();}
-
-
 
     void print9_upper()     {line();}
     void print9_uphalf()    {line();}
