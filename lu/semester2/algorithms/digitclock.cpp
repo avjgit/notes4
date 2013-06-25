460da0434385dccc340010e002194c867c671fb6
avjgit <avjmail@gmail.com>
2013-06-26 00:02:08 +0300

    lu alogrithms: lab clock: refactored lines

--- lu/semester2/algorithms/digitclock.cpp
+++ lu/semester2/algorithms/digitclock.cpp
@@ -17,128 +17,104 @@ public:
         for (int i = 0; i < size; i++) cout << "-";
         cout << " " << endl;
     }
+    void double_line()
+    {
+        cout << "|";
+        for (int i = 0; i < size; i++) cout << " ";
+        cout << "|" << endl;
+    }
+    void right_line()
+    {
+        cout << " ";
+        for (int i = 0; i < size; i++) cout << " ";
+        cout << "|" << endl;
+    }
+    void left_line()
+    {
+        cout << "|";
+        for (int i = 0; i < size; i++) cout << " ";
+        cout << " " << endl;
+    }
+
     void print0(){
         //upper line
         line();
         //first half
         for (int i = 0; i < size; i++){
-            cout << "|";
-            for (int i = 0; i < size; i++) cout << " ";
-            cout << "|" << endl;
+            double_line();
         }
         //middle line
-        cout << "|";
-        for (int i = 0; i < size; i++) cout << " ";
-        cout << "|" << endl;
+        double_line();
         //second half
         for (int i = 0; i < size; i++){
-            cout << "|";
-            for (int i = 0; i < size; i++) cout << " ";
-            cout << "|" << endl;
+            double_line();
         }
         // bottom line
         line();
     }
     void print1(){
         //upper line
-        cout << " ";
-        for (int i = 0; i < size; i++) cout << " ";
-        cout << "|" << endl;
+        right_line();
         //first half
         for (int i = 0; i < size; i++){
-            cout << " ";
-            for (int i = 0; i < size; i++) cout << " ";
-            cout << "|" << endl;
+            right_line();
         }
         //middle line
-        cout << " ";
-        for (int i = 0; i < size; i++) cout << " ";
-        cout << "|" << endl;
+        right_line();
         //second half
         for (int i = 0; i < size; i++){
-            cout << " ";
-            for (int i = 0; i < size; i++) cout << " ";
-            cout << "|" << endl;
+            right_line();
         }
         // bottom line
-        cout << " ";
-        for (int i = 0; i < size; i++) cout << " ";
-        cout << "|" << endl;
+        right_line();
     }
     void print2(){
-        //upper line
-        // cout << "-";
-        // for (int i = 0; i < size; i++) cout << "-";
-        // cout << "-" << endl;
+        // upper line
         line();
-
         //first half
         for (int i = 0; i < size; i++){
-            cout << " ";
-            for (int i = 0; i < size; i++) cout << " ";
-            cout << "|" << endl;
+            right_line();
         }
         //middle line
-        // cout << "-";
-        // for (int i = 0; i < size; i++) cout << "-";
-        // cout << "-" << endl;
         line();
-
         //second half
         for (int i = 0; i < size; i++){
-            cout << "|";
-            for (int i = 0; i < size; i++) cout << " ";
-            cout << " " << endl;
+            left_line();
         }
         // bottom line
         line();
-
     }
     void print3(){
         //upper line
         line();
         //first half
         for (int i = 0; i < size; i++){
-            cout << " ";
-            for (int i = 0; i < size; i++) cout << " ";
-            cout << "|" << endl;
+            right_line();
         }
         //middle line
         line();
         //second half
         for (int i = 0; i < size; i++){
-            cout << " ";
-            for (int i = 0; i < size; i++) cout << " ";
-            cout << "|" << endl;
+            right_line();
         }
         // bottom line
         line();
-
     }
     void print4(){
         //upper line
-        cout << "|";
-        for (int i = 0; i < size; i++) cout << " ";
-        cout << "|" << endl;
+        double_line();
         //first half
         for (int i = 0; i < size; i++){
-            cout << "|";
-            for (int i = 0; i < size; i++) cout << " ";
-            cout << "|" << endl;
+            double_line();
         }
         //middle line
         line();
-
         //second half
         for (int i = 0; i < size; i++){
-            cout << " ";
-            for (int i = 0; i < size; i++) cout << " ";
-            cout << "|" << endl;
+            right_line();
         }
         // bottom line
-        cout << " ";
-        for (int i = 0; i < size; i++) cout << " ";
-        cout << " " << endl;
+        right_line();
     }
 };
 
