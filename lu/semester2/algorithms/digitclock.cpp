536f5255f1378daefa93f27f0d6a3361555d2e76
avjgit <avjmail@gmail.com>
2013-06-25 23:14:29 +0300

    lu alogrithms: lab clock: size refactored

--- lu/semester2/algorithms/digitclock.cpp
+++ lu/semester2/algorithms/digitclock.cpp
@@ -4,31 +4,30 @@ using namespace std;
 
 class Clock
 {
-    int left_border;
-    int right_border;
-    int up_border;
-    int down_border;
-    int middle_line;
-    int width;
-    int height;
-
+    // int left_border;
+    // int right_border;
+    // int up_border;
+    // int down_border;
+    // int middle_line;
+    // int width;
+    // int height;
+    int size;
 public:
     Clock()
     {
-        left_border = 1;
-        right_border = 1;
-        up_border = 1;
-        down_border = 1;
-        middle_line = 1;
+        size = 1;
+        // left_border = 1;
+        // right_border = 1;
+        // up_border = 1;
+        // down_border = 1;
+        // middle_line = 1;
     }
-    Clock(int size)
+    Clock(int size_)
     {
-        *this = Clock();
-        width = left_border + size + right_border;
-        height = up_border + size + middle_line + size + down_border;
+        size = size_;
     }
 
-    void print0(int size)
+    void print0()
     {
         cout << " ";
         for (int i = 0; i < size; i++) cout << "-";
@@ -51,6 +50,6 @@ public:
 int main()
 {
     Clock clock = Clock(1);
-    clock.print0(1);
+    clock.print0();
     return 0;
 }
