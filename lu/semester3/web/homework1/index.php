acfa5386bcf7ef28d301407787dd9d0a31b17970
avjgit <avjmail@gmail.com>
2013-09-25 00:39:07 +0300

    lu web hw1 getting input

--- lu/semester3/web/homework1/index.php
+++ lu/semester3/web/homework1/index.php
@@ -9,8 +9,24 @@
 //Your code goes here
 
 
+if(isset($_GET['amount'])) {
+    $amount = $_GET["amount"];
+}
+
+if(isset($_GET['source'])) {
+    $source = $_GET["source"];
+}
+
+if(isset($_GET['target'])) {
+    $target = $_GET["target"];
+    echo "You asked to convert $amount $source into $target";
+}
+
+
 $result_status = ""; //valid values: empty string, "success", "error"
 $result_message = "";
 $target_currencies = array('LVL', 'GBP', 'RUB', 'CHF', 'SEK', 'NOK', 'JPY', 'LTL');
 
 require("view.php");
+
+?>
