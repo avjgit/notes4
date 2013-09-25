3811e7d0edef434945d928bff9b1efd3bc12b520
avjgit <avjmail@gmail.com>
2013-09-25 19:11:13 +0300

    lu web hw1 target connection error handling

--- lu/semester3/web/homework1/index.php
+++ lu/semester3/web/homework1/index.php
@@ -1,4 +1,9 @@
 <?php
+
+$result_status = ""; //valid values: empty string, "success", "error"
+$result_message = "";
+$target_currencies = array('LVL', 'GBP', 'RUB', 'CHF', 'SEK', 'NOK', 'JPY', 'LTL');
+
 if(
 	isset($_GET['amount']) and
 	isset($_GET['source']) and
@@ -11,30 +16,50 @@ if(
 
     if ($target == 'EUR'){    	
 		$XML=simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
-		        
-		foreach($XML->Cube->Cube->Cube as $rate){
-			if ($rate["currency"] == $source){
-				echo 'rate is ', $rate["rate"];
-				// echo 'hey, this would be ', ($amount * 100000) / ($rate["rate"] * 100000), ' in euro!';
+
+		if(isset($XML)){
+			foreach($XML->Cube->Cube->Cube as $rate){
+				if ($rate["currency"] == $source){
+					echo 'rate is ', $rate["rate"];
+					$result_status = "success";
+					$result_message = ($amount * 100000) / ($rate["rate"] * 100000);
+					// echo 'hey, this would be ', $result_message, ' in euro!';
+				}
+			}   			
+			if(!isset($result_status)){
+				$result_status = "error";
+				$result_message = "Got currency data, but there is no rate for your currency.";
 			}
-		}    
+		}
+		else {
+			$result_status = "error";
+			$result_message = "Could not get currency data.";
+		}        
+ 
     }
     elseif ($target == 'LVL') {
 		$XML=simplexml_load_file("http://www.bank.lv/vk/xml.xml");
-    
-		// foreach(...){
-		// 	if ($rate["currency"] == $target){
-		// 		echo 'rate is ', $rate["rate"];
-				// echo 'hey, this would be ', $amount * $rate["rate"], ' in lats!';		
-		// 	}
-		// }       	
-    	 
+
+		if(isset($XML)){
+			foreach(...){
+				if ($rate["currency"] == $source){
+					echo 'rate is ', $rate["rate"];
+					$result_status = "success";
+					$result_message = $amount * $rate["rate"]
+					echo 'hey, this would be ', $result_message, ' in lats!';		
+				}
+			}      						
+			if(!isset($result_status)){
+				$result_status = "error";
+				$result_message = "Got currency data, but there is no rate for your currency.";
+			}
+		}
+		else {
+			$result_status = "error";
+			$result_message = "Could not get currency data.";
+		} 		
     }       
 }
-
-$result_status = ""; //valid values: empty string, "success", "error"
-$result_message = "";
-$target_currencies = array('LVL', 'GBP', 'RUB', 'CHF', 'SEK', 'NOK', 'JPY', 'LTL');
 
 require("view.php");
 
