8f14ee3e3ebcd69f9d6b8652a3399683947dd3b7
avjgit <avjmail@gmail.com>
2013-09-25 18:47:59 +0300

    lu web hw1 calculation

--- lu/semester3/web/homework1/index.php
+++ lu/semester3/web/homework1/index.php
@@ -20,13 +20,19 @@ if(isset($_GET['source'])) {
 if(isset($_GET['target'])) {
     $target = $_GET["target"];
     echo "You asked to convert $amount $source into $target";
+
+	$XML=simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+	        
+	foreach($XML->Cube->Cube->Cube as $rate){
+		if ($rate["currency"] == $target){
+			echo 'rate is ', $rate["rate"];
+			// echo 'hey, this would be ', ($amount * 100000) / ($rate["rate"] * 100000), ' in euro!';
+		}
+	    // echo '1&euro;='.$rate["rate"].' '.$rate["currency"].'<br/>';
+	}    
 }
 
-$XML=simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
-        
-foreach($XML->Cube->Cube->Cube as $rate){
-    echo '1&euro;='.$rate["rate"].' '.$rate["currency"].'<br/>';
-}
+
 
 
 $result_status = ""; //valid values: empty string, "success", "error"
