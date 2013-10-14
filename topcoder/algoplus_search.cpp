d9f190e84ef46da05ecc47f791002898be2873d7
avjgit <avjmail@gmail.com>
2013-10-14 10:17:44 +0300

    algo+ search read bugfix

--- topcoder/algoplus_search.cpp
+++ topcoder/algoplus_search.cpp
@@ -2,24 +2,49 @@
 #include <fstream>
 using namespace std;
 
+bool is_end_of_word(char c)
+{
+    char separators[] = {' ', '.', ',', '(', ')', '!', '?'};
+    int arrSize = sizeof(separators) / sizeof(char);
+    for (int i = 0; i < arrSize; i++)
+    {
+        if (separators[i] == c)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 struct searchchar{
     char c;
     searchchar* next;
 };
 
+struct found{
+    int position;
+    found* next;
+};
 
 int main()
 {
     ifstream searchfile("word.in");
+    ofstream outfile("out.out");
+
+    found* curr_found;
+    found* prev_found;
+    found* first_found = NULL;
 
     searchchar* current;
     searchchar* previous;
     searchchar* first = NULL;
+    int length = 0;
 
     char curc;
-    while(searchfile.good())
+    searchfile >> curc;
+    while(!searchfile.eof())
     {
-        searchfile >> curc;
+        length++;
 
         current = new searchchar;
         current->c = curc;
@@ -32,6 +57,8 @@ int main()
             previous->next = current;
         }
         previous = current;
+
+        searchfile >> curc;
     }
 
     current = first;
@@ -46,10 +73,48 @@ int main()
     {
         infile >> curc;
 
+        current = first;
+        for(int i = 0; i < length; i++)
+        {
+            if(curc == current->c){
+                current = current->next;
+                if (i == length-1){
+                    infile >> curc;
+                    if (is_end_of_word(curc)){
 
+                        curr_found = new found;
+                        curr_found->position = i;
+                        curr_found->next = NULL;
 
+                        if (first_found == NULL) {
+                            first_found = curr_found;
+                        }
+                        else {
+                            prev_found->next = curr_found;
+                        }
+                        prev_found = curr_found;
+                    }
+                }
+            }
+            else{
+                while(!is_end_of_word(curc))
+                    infile >> curc;
+                break;
+            }
+        }
     }
 
+    // char cfound = first_found->position;
+    // cout << cfound;
+
+    // curr_found = first_found;
+    // while(curr_found->next != NULL)
+    // {
+    //     // cout << "found " << curr_found->position;
+    //     // outfile << curr_found->position;
+    //     // outfile << " ";
+    //     curr_found = curr_found->next;
+    // }
 
     infile.close();
     return 0;
--- topcoder/word.in
+++ topcoder/word.in
@@ -1,1 +1,1 @@
-test
+tesx
