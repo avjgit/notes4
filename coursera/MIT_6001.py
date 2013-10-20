72adafe0567fbba0e9ab6bb8ec14723a49d596aa
avjgit <avjmail@gmail.com>
2013-10-20 23:18:19 +0300

    MIT 6.00.1 week 1

---
+++ coursera/MIT_6001.py
@@ -0,0 +1,9 @@
+# week 1, task 11
+if (type(varA) == str or type(varB) == str):
+    print('string involved')
+elif (varA > varB):
+    print('bigger')
+elif (varA == varB):
+    print('equal')
+elif (varA < varB):
+    print('smaller')
