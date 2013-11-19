1e3fd6b9ad36a252766304f05e215db4bd39b58b
avjgit <avjmail@gmail.com>
2013-11-19 11:11:41 +0200

    read without duplicates

--- lu/semester3/algorithms/03_mirrowgraph/spogulis.cpp
+++ lu/semester3/algorithms/03_mirrowgraph/spogulis.cpp
@@ -5,19 +5,19 @@ int main()
 {
     FILE* in;
     FILE* out;
-    // todo: tests to pass: 1 2 3 4 5 6 7 8
+    // todo: tests to pass 1 by 1: 2 3 4 5 6 7 8
+    // todo: tests to pass with final: 1 2 3 4 5 6 7 8
     //todo: real name: "spogulis.in"
-    in = fopen("spogulis.i1.txt", "r");
+    in = fopen("spogulis.i2.txt", "r");
     out = fopen("spogulis.out", "w+");
     int node = 0;
 
-    // while (!feof(in))
-    // {
-    	// fgetc( in);
-    	fscanf(in, "%i", &node);
-    	fprintf(out, "%s", "node is ");
-    	fprintf(out, "%i", node);
-    // }
+    fscanf(in, "%i", &node);
+    while (!feof(in))
+    {
+    	fprintf(out, "%i\n", node);
+        fscanf(in, "%i", &node);
+    }
 
     fclose(in);
     fclose(out);
