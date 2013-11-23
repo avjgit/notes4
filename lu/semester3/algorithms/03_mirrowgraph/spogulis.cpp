10334e2fef70d6f6ee0ec62067c13ed8d04f8fb9
avjgit <avjmail@gmail.com>
2013-11-23 16:38:42 +0200

    renamings

--- lu/semester3/algorithms/03_mirrowgraph/spogulis.cpp
+++ lu/semester3/algorithms/03_mirrowgraph/spogulis.cpp
@@ -1,53 +1,43 @@
 #include "stdio.h"
-// #include "stdlib.h"
-//todo: remove
-// #include <iostream>
+#include "stdlib.h"
+
 #include <fstream>
-// #include <string>
 #include <sstream>
 using namespace std;
-//todo: end of remove
-
-int main()
+struct element
 {
-    ////////////////////////////////////////////////////////// implementation 1
-    // FILE* in;
-    // FILE* out;
-    // todo: tests to pass 1 by 1: 2 3 4 5 6 7 8
-    // todo: tests to pass with final: 1 2 3 4 5 6 7 8
-    // todo: real name: "spogulis.in"
-    // todo: correct formatting
-    // in = fopen("spogulis.i2.txt", "r");
-    // out = fopen("spogulis.out", "w+");
-    // int node = 0;
-
-    // fscanf(in, "%i", &node);
-    // Ieejas failu vienmēr noslēdz rindiņa, kurā ir tikai skaitlis 0.
-    // while (node == 0)
-    // while (!feof(in))
-    // {
-        // fprintf(out, "%i\n", node);
-        // fscanf(in, "%i", &node);
-    // };
-    // fclose(in);
-    // fclose(out);
-    ////////////////////////////////////////////////////////// implementation 2
+    int value;
+    element *last_leaf, *siebling;
+};
+int main(){
     ifstream in("spogulis.in");
     ofstream out("spogulis.out");
     string line;
-    int leaf, node;
-    while (getline(in, line))
-    {
+    int node, leaf;
+    element *root = NULL;
+    element *current = NULL;
+    element *previous = NULL;
+
+
+    while (getline(in, line)){
         istringstream linestream(line);
         linestream >> node;
-        out << node << " has: ";
-        while( linestream >> leaf ) {
-            out << leaf << " ";
+        if (root == NULL) {
+            root = new element;
+            root->value = node;
         }
-        out << "\n";
+        else{
+
+        }
+        // out << node_value << " ";
+        while( linestream >> leaf ) {
+            // out << leaf << " ";
+            current = new element;
+            current->value = leaf;
+
+        }
+
+        // out << "\n";
     }
-
-    fprintf(stdout, "Sis kods veiksmigi kompilejas. Pedejais mezgls bija %i\n", node);
-
     return 0;
 }
