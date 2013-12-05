fdef71c78475311fdc8026bdc074b518b1466e2a
avjgit <avjmail@gmail.com>
2013-12-05 18:45:38 +0200

    reading flight

--- lu/semester3/algorithms/04_flights/lidostas.cpp
+++ lu/semester3/algorithms/04_flights/lidostas.cpp
@@ -6,21 +6,11 @@ using namespace std;
 struct flight{
     int departure_time;
     int arrival_airport;
-    itn arrival_time;
-    flight* next_flight;
+    int arrival_time;
     bool used;
+    flight* next_flight;
 };
-
 int main(){
-    // for 2d flights array, for columns (as departure times)
-    const int DAY_HOURS = 24;
-    const int HOUR_MINUTES = 60;
-    const int DAY_MINUTES = DAY_HOURS * DAY_MINUTES;
-    const int END_OF_INPUT = 0;
-    const char MM_SEPARATOR = ':';
-    const char TIME_SEPARATOR = '-';
-
-    // cout << DAY_MINUTES;
     int airports, start_airport, arrival_airport, departure_airport, target_airport;
     int flights, arrival_time, departure_time;
     int start_HH, start_MM;
@@ -28,79 +18,26 @@ int main(){
     int departure_HH, departure_MM;
     string flight_line;
 
-
-
-    // ifstream in("lidostas.in");
-    // ofstream out("lidostas.out");
-
-    // in >> airports;
-    // in >> start_airport;
-    // in >> target_airport;
-
-    // out << "ok, there are " << airports << " airports.";
-    // out << "we need to get from " << start_airport << " to " << target_airport << "\n";
-
-
     FILE* in = fopen("lidostas.in", "r");
     FILE* out = fopen("lidostas.out", "w+");
+    // starting data
     fscanf  (in, "%i", &airports);
-    fprintf (out, "%i", airports);
-
     fscanf  (in, "%i", &start_airport);
     fscanf  (in, "%i", &target_airport);
-
-    fprintf (out, "%i", start_airport);
-    fprintf (out, "%i", target_airport);
-
-
     fscanf  (in, "%i:%i", &start_HH, &start_MM);
-    // fscanf  (in, "%i", &start_MM);
 
-    fprintf (out, "%i", start_HH);
-    fprintf (out, "%i", start_MM);
+    fprintf (out, "%i\n", airports);
+    fprintf (out, "%i\n", start_airport);
+    fprintf (out, "%i\n", target_airport);
+    fprintf (out, "%02i:%02i", start_HH, start_MM);
 
-
-    fscanf  (in, "%i", &departure_airport);
-    fscanf  (in, "%i", &arrival_airport);
+    // flights data
+    fscanf  (in, "%i %i %i", &departure_airport, &arrival_airport, &flights);
 
     fprintf (out, " from %i", departure_airport);
     fprintf (out, " to %i", arrival_airport);
-// int x;
+    fprintf (out, " there will be %i flights", flights);
 
-//     in >> departure_airport;
-//     in >> x;
-//     out << "we arriving at ";
-//     out << std::setw(2) << std::setfill('0') << departure_airport;
-//     out << ":";
-//     out << std::setw(2) << std::setfill('0') << x;
-    // while(true){
-        // in >> start_MM;
-        // if (departure_airport == END_OF_INPUT)
-            // break;
-        // in >> start_HH;
-        // in >> flights;
-        // in >> departure_HH;
-        // in >> departure_MM;
-        // in >> arrival_HH;
-        // in >> arrival_MM;
-    // }
-    // out << "ok";
-        // out << "\n" << start_MM ;
-        // out << start_HH << "\n";
-        // out << flights << "\n";
-        // out << departure_HH << ":";
-        // out << departure_MM << "-";
-        // out << arrival_HH << ":";
-        // out << arrival_MM;
-
-
-
-
-
-    // while (getline(in, line)){
-        // istringstream linestream(flight_line);
-//
-    // }
 
     return 0;
 }
