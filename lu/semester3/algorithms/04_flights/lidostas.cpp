d26436f8de16d2206924c3fc1f1bd6ec5bbb51f2
avjgit <avjmail@gmail.com>
2013-12-07 14:11:50 +0200

    reading flights

--- lu/semester3/algorithms/04_flights/lidostas.cpp
+++ lu/semester3/algorithms/04_flights/lidostas.cpp
@@ -5,14 +5,14 @@
 #include <string>
 using namespace std;
 struct flight{
-    int departure_time;
     int arrival_airport;
+    int departure_time;
     int arrival_time;
     bool used;
     flight* other_flight;
 };
 int main(){
-    int airports, start_airport, arrival_airport, departure_airport, target_airport;
+    int airports_count, start_airport, arrival_airport, departure_airport, target_airport;
     int flights, arrival_time, departure_time;
     int start_HH, start_MM;
     int arrival_HH, arrival_MM;
@@ -21,11 +21,14 @@ int main(){
 
     FILE* in = fopen("lidostas.in", "r");
     FILE* out = fopen("lidostas.out", "w+");
-    // starting data
-    fscanf  (in, "%i", &airports);
+
+    // reading main data
+    fscanf  (in, "%i", &airports_count);
     fscanf  (in, "%i", &start_airport);
     fscanf  (in, "%i", &target_airport);
     fscanf  (in, "%i:%i", &start_HH, &start_MM);
+
+    flight* airports[airports_count + 1]; // "+1" - just for easier reference in later ([n], not [n-1])
 
     // 1d array of pointers to airports first flights
     // each flight then has flag, if USED, and pointer to next flight
@@ -36,48 +39,70 @@ int main(){
     // pie tam no pieejamiem
     // un jaizvelas mazakais
 
-    string s = "asdf";
-    // for(int i = 0; i < 9; i++){
-    int i = 0;
+    // reading flights data
+    flight *f, *last_flight;
     while(true){
-        i++;
         fscanf(in, "%d", &departure_airport);
+        // stop if 0
         if (departure_airport == 0)
             break;
-        fscanf(
-            in,
-            "%d %d %d:%d-%d:%d\n",
-            &arrival_airport,
-            &flights,
-            &departure_HH,
-            &departure_MM,
-            &arrival_HH,
-            &arrival_MM
-        );
-        fprintf(
-            out,
-            "flight %d: %d %d %d %02d:%02d-%02d:%02d\n",
-            i,
-            departure_airport,
-            arrival_airport,
-            flights,
-            departure_HH,
-            departure_MM,
-            arrival_HH,
-            arrival_MM
-        );
+
+        fscanf(in, "%d %d", &arrival_airport, &flights);
+
+        for (int i = 1; i <= flights; i++){
+
+            fscanf(
+                in,
+                "%d:%d-%d:%d",
+                &departure_HH,
+                &departure_MM,
+                &arrival_HH,
+                &arrival_MM
+            );
+
+        // struct flight{
+        // int departure_time;
+        // int arrival_airport;
+        // int arrival_time;
+        // bool used;
+        // flight* other_flight;
+
+            f = new flight;
+            f->arrival_airport = arrival_airport;
+            // todo: fix correct time; this is just to quick check of idea
+            f->departure_time = departure_HH * 100 + departure_MM;
+            f->arrival_time = arrival_HH * 100 + arrival_MM;
+            f->used = false;
+
+            if (airports[departure_airport] == NULL){
+                airports[departure_airport] = f;
+            }
+            else{
+                last_flight->other_flight = f;
+            }
+            last_flight = f;
+        }
+
+        // fprintf(
+        //     out,
+        //     "flight %d: %d %d %d %02d:%02d-%02d:%02d\n",
+        //     i,
+        //     departure_airport,
+        //     arrival_airport,
+        //     flights,
+        //     departure_HH,
+        //     departure_MM,
+        //     arrival_HH,
+        //     arrival_MM
+        // );
     }
-    fprintf(out, "%s\n", s.c_str());
 
-    fclose(out);
-    out = fopen("lidostas.out", "w");
-    fprintf(out, "%s\n", s.c_str());
+    // fclose(out);
+    // out = fopen("lidostas.out", "w");
+    // fprintf(out, "%s\n", "Impossible");
 
-
-
-
-    fclose(in);
-    fclose(out);
+    // fclose(in);
+    // fclose(out);
 
     return 0;
 }
