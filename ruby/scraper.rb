5def8790b1e2474897c26edf6795b4b694af9f4b
avjgit <avjmail@gmail.com>
2013-12-08 05:00:40 -0800

    done

--- ruby/scraper.rb
+++ ruby/scraper.rb
@@ -2,17 +2,32 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-# source = "http://nms.lu.lv/goda-plaksne/kopvertejuma-labakie/"
-source = "http://nms.lu.lv/skolenu-ranga-tabula-2011-12-m-g/"
-page  = Nokogiri::HTML(open(source).read)
-# p page.class
-# p page
-# p page.css('td')[1] # should return an array of 4 table cell nodes
-# a = page.xpath("/html/body/table/tr[1]").text
-# p a
-rows = page.xpath('//table/tbody/tr[position() > 1]')
-for r in rows
-	p r.xpath('td')[1].text
+source_template = 'http://nms.lu.lv/skolenu-ranga-tabula-STARTYEAR-ENDYEAR-m-g/'
+
+ratings_per_year = Hash.new
+ratings_all = Hash.new 0
+
+for year in 1982..2011
+
+	source = source_template.sub 'STARTYEAR', year.to_s
+	
+	if (year == 1999) # because of nms webmasters...
+		source = source.sub 'ENDYEAR', (year + 1).to_s
+	else
+		source = source.sub 'ENDYEAR', ((year + 1).to_s)[2..3]
+	end
+	page  = Nokogiri::HTML(open(source).read)
+	
+	year_ratings = Hash.new 0
+	schools = page.xpath('//table/tbody/tr[position() > 1]')
+	for row in schools
+		school =  row.xpath('td')[1].text
+		year_ratings[school] += 1
+		ratings_all[school] += 1
+	end
+	ratings_per_year[year] = year_ratings
+
 end
-# rows.xpath('td').map {|td| td.text}
-# p rows
+
+# p ratings_per_year
+p ratings_all
