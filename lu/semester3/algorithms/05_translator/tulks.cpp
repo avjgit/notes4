e16cf4d39961d97573df9723a8165e1a5619d16f
avjgit <avjmail@gmail.com>
2013-12-27 13:48:23 +0200

    read till break

--- lu/semester3/algorithms/05_translator/tulks.cpp
+++ lu/semester3/algorithms/05_translator/tulks.cpp
@@ -8,27 +8,34 @@ int main() {
     FILE* in = fopen("tulks.in", "r");
     FILE* out = fopen("tulks.out", "w+");
 
+    //todo
+    // rootA, rootB, rootStart default = rootA
+
 // ################################# Ieeja:
 // Ieejas fails satur 3 datu blokus: vārdnīcu, tulkošanas virzienu un tulkojamo tekstu.
 // ============ Vārdnīca sastāv no vārdu pārīšiem formā <Word_A> <Word_B>
     char word_a[20];
     char word_b[20];
 
+    for(;;){
+
     fscanf(in, "%s", word_a);
     fprintf(out, "%s%s", word_a, " ");
-
-    fscanf(in, "%s", word_b);
-    fprintf(out, "%s%s", word_b, " ");
-
-    // fscanf(in, "%s", word_a);
-    // fprintf(out, "%s%s", word_a, " ");
-
-    // fscanf(in, "%s", word_b);
-    // fprintf(out, "%s%s", word_b, " ");
 
     for(int i = 0; word_a[i] != '\0'; i++)
         fprintf(stdout, "%c", word_a[i], " ");
 
+    if (word_a[0] == '-'){ // word is '-->', so end of dictionary
+        // todo
+        // if (word[2] == '<')
+            // rootStart = rootB;
+        break;
+    }
+
+    fscanf(in, "%s", word_b);
+    fprintf(out, "%s%s", word_b, " ");
+
+    }
 
 // Vārdnīcā ir vismaz viens vārdu pāris, bet ne vairāk kā 50’000 vārdu pāru.
 // Ir zināms, ka vārdnīcā visi vārdi valodā A ir unikāli, t.i. tie nesatur dublikātus. Arī vārdnīcas B vārdi ir unikāli.
